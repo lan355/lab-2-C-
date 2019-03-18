2b5efea37bf702312531aa1783c59da2ef769874
laalan467@gmail.com <laalan467@gmail.com>
2019-03-18 16:51:51 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_numbers
+{
+    class Complex
+    {
+         double module;
+         double argument;
+
+        public Complex(double module, double argument)
+        {
+            this.module = module;
+            this.argument = argument;
+        }
+
+        public double Module { get => module; set => module = value; }
+        public double Argument { get => argument; set => argument = value; }
+
+        public override string ToString()
+        {
+            return "Тригонометрическая форма: " + module.ToString() + "*(Cos(" + argument.ToString() + ")+i*Sin(" + argument.ToString() + "))" + "\n" +
+                "Алгебраическая форма: " + Math.Round(this.module * Math.Cos(this.argument), 2) + "+" + Math.Round(this.module * Math.Sin(this.argument), 2) + "i";
+
+        }
+
+        public static Complex operator *(Complex x, Complex y)
+        {
+            return new Complex (x.Module * y.Module, x.Argument + y.Argument);
+        }
+
+        public static Complex operator /(Complex x, Complex y)
+        {
+            return new Complex(Math.Round(x.Module / y.Module,2), x.Argument - y.Argument);
+        }
+
+        public static Complex operator +(Complex x, Complex y)
+        {
+            return new Complex(Math.Round(x.Module * Math.Cos(x.Argument), 2) + Math.Round(y.Module * Math.Cos(y.Argument), 2), Math.Round(x.Module * Math.Sin(x.Argument), 2) + Math.Round(y.Module * Math.Sin(y.Argument), 2));
+        }
+
+        public static Complex operator -(Complex x, Complex y)
+        {
+            return new Complex(Math.Round(x.Module * Math.Cos(x.Argument), 2) - Math.Round(y.Module * Math.Cos(y.Argument), 2), Math.Round(x.Module * Math.Sin(x.Argument), 2) - Math.Round(y.Module * Math.Sin(y.Argument), 2));
+        }
+
+
+
+
+
+        public void Print()
+        {
+            double a = Math.Sqrt((this.module) * (this.module) + (this.argument) * (this.argument));
+            double b = Math.Atan(this.argument / this.module) + Math.PI;
+            Console.WriteLine(String.Format("a={0}+{1}i", this.Module, this.Argument));
+            Console.WriteLine(String.Format("a={0}(cos{1}+sin{1})", a, b));
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Complex_numbers
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double module2;
+            Console.WriteLine("Введите Module1= ");
+            double module = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Argument1= ");
+            int argument = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Введите Module2= ");
+                module2 = Convert.ToDouble(Console.ReadLine());
+                if (module2 == 0)
+                    Console.WriteLine("На ноль делить нельзя, введите заново");
+            }
+            while (module2 == 0);
+            Console.WriteLine("Введите Argument2= ");
+            int argument2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Complex x = new Complex(module, argument);
+            Complex y = new Complex(module2, argument2);
+            Complex um = x * y;
+            Complex del = x / y;
+            Complex pl = x + y;
+            Complex min = x - y;
+            Complex k = new Complex(module, argument);
+
+            Console.WriteLine("x=" + x.Module + "(Cos(" + x.Argument + ")+iSin(" + x.Argument + ")");
+            Console.WriteLine("y=" + y.Module + "(Cos(" + y.Argument + ")+iSin(" + y.Argument + ")");
+            Console.WriteLine("Умножение " + "\n" + um.ToString());
+            Console.WriteLine("Деление " + "\n" + del.ToString());
+            Console.WriteLine("Сложение " + "\n" + pl.ToString());
+            Console.WriteLine("Вычитание " + "\n" + min.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
